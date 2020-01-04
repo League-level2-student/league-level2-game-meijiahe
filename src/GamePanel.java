8c313b810151e1f867f9fc386752728fb31a7bd5
league <league@imac-9.attlocal.net>
2020-01-04 11:25:52 -0800

    GamePanelandObjectManager

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,13 +1,69 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements KeyListener {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 Timer frameDraw;
 Timer blockSpawn;
-Paddle paddle = new Paddle();
+Paddle paddle = new Paddle(50,50,60,10);
+ObjectManager objectmanager = new ObjectManager();
+final int MENU = 0;
+final int GAME = 1;
+final int END = 2;
+int currentState = MENU;
+Font titleFont = new Font ("Arial", Font.PLAIN, 48);
+Font smallTitleFont = new Font ("Arial", Font.PLAIN, 20);
+Font middleTitleFont = new Font ("Arial", Font.PLAIN, 30);
+@Override
+public void paintComponent(Graphics g) {
+	if(currentState == MENU){
+	    drawMenuState(g);
+	}else if(currentState == GAME){
+	    drawGameState(g);
+	}else if(currentState == END){
+	    drawEndState(g);
+	}
+}
+void updateMenuState() {  
+}
+void updateGameState() {  
+	objectmanager.update();
+	if (paddle.isActive==true) { 
+	}
+	else {
+		currentState = END;
+	}
+}
+void updateEndState()  {  
+	 
+}
+	private void drawEndState(Graphics g) {
+	// TODO Auto-generated method stub
+	
+}
+	private void drawGameState(Graphics g) {
+	// TODO Auto-generated method stub
+	
+}
+	private void drawMenuState(Graphics g) {
+	// TODO Auto-generated method stub
+		g.setColor(Color.WHITE);
+		 g.fillRect(0, 0, breakout.WIDTH, breakout.HEIGHT);
+		 g.setFont(titleFont);
+		 g.setColor(Color.RED);
+		 g.drawString("BREAKOUT", 22, 100);
+		 g.setFont(smallTitleFont);
+		 g.drawString("Press ENTER to start", 150, 400);
+		 g.setFont(middleTitleFont);
+		 g.drawString("Press SPACE for instructions", 50, 600);
+}
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -22,6 +78,12 @@ Paddle paddle = new Paddle();
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ObjectManager {
+public class ObjectManager implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
